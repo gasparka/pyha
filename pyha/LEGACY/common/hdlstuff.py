f262c73399cea03ab8ec514b7fd897b9e0b952ae
Gaspar <gkarm@live.com>
2016-10-16 16:41:43 +0300

    remove legacy code

--- pyha/LEGACY/common/hdlstuff.py
+++
@@ -1,7 +0,0 @@
-class HDLStuff(object):
-    def __init__(self, base_path, vhdl_src, verilog_src, input_sfix, output_sfix):
-        self.output_sfix = output_sfix
-        self.input_sfix = input_sfix
-        self.base_path = base_path
-        self.vhdl_src = vhdl_src
-        self.verilog_src = verilog_src
