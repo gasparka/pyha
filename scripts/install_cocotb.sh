196e4ec870518324ecef607c79d2b875364880a3
gaspar <gkarm@live.com>
2016-10-17 19:10:54 +0300

    add install scripts

--- scripts/install_cocotb.sh
+++ scripts/install_cocotb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -evx
+set -vx
 # TODO: create PR and use original cocotb repo!
 git clone https://github.com/petspats/cocotb
--- scripts/install_ghdl.sh
+++ scripts/install_ghdl.sh
@@ -7,13 +7,6 @@ apt-get update
 apt-get install -y gnat-4.6-base libgnat-4.6
 rm -f /etc/apt/sources.list.d/pyha_tmp.list
 
-# install gnat runtime, adding repo is needed for Ubuntu > 14
-bash -c 'echo deb cz.archive.ubuntu.com/ubuntu precise main universe > /etc/apt/sources.list.d/pyha_tmp.list'
-apt-get update
-apt-get install -y gnat-4.6-base, libgnat-4.6
-rm -f /etc/apt/sources.list.d/pyha_tmp.list
-
-
 # download ghdl
 wget https://github.com/tgingold/ghdl/releases/download/2016-09-14/ghdl-0.34dev-mcode-2016-09-14.tgz -O /tmp/ghdl.tar.gz
 mkdir ghdl
