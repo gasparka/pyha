88b5fd37536537badecdefa97a1bcf4ad08a8b50
gaspar <gkarm@live.com>
2016-10-17 19:10:54 +0300

    add install scripts

--- scripts/install_ghdl.sh
+++ scripts/install_ghdl.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -evx
+set -vx
+
+# install gnat runtime, adding repo is needed for Ubuntu > 14
+bash -c 'echo deb http://cz.archive.ubuntu.com/ubuntu precise main universe precise main universe > /etc/apt/sources.list.d/pyha_tmp.list'
+apt-get update
+apt-get install -y gnat-4.6-base libgnat-4.6
+rm -f /etc/apt/sources.list.d/pyha_tmp.list
 
 # install gnat runtime, adding repo is needed for Ubuntu > 14
 bash -c 'echo deb cz.archive.ubuntu.com/ubuntu precise main universe > /etc/apt/sources.list.d/pyha_tmp.list'
