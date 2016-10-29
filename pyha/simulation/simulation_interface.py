10b8856340558cebc2c9f8d8ed3f9353831286be
Gaspar <gkarm@live.com>
2016-10-29 14:52:49 +0300

    start simulation_interface class

---
+++ pyha/simulation/simulation_interface.py
@@ -0,0 +1,5 @@
+
+
+class Simulation:
+    def __init__(self):
+        pass
---
+++ tests/test_simulation_interface.py
@@ -0,0 +1,18 @@
+import subprocess
+
+import pyha
+
+
+def test_ghdl_version():
+    # ghdl has same version for 'all versions', a bit shit
+    ret = subprocess.getoutput('ghdl --version | grep -m1 GHDL')
+    assert 'GHDL 0.34dev (20151126) [Dunoon edition]' == ret
+
+
+def test_cocotb_version():
+    version_file = pyha.__path__[0] + '/../cocotb/version'
+    with open(version_file, 'r') as f:
+        assert 'VERSION=1.0\n' == f.read()
+
+
+
