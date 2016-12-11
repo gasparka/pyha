89812eec86d37a1c842cead2825549211977e770
gaspar <gkarm@live.,com>
2016-12-11 14:53:54 +0200

    refactor duplicate function

--- tests/test_simulator_rtl_match.py
+++ tests/test_simulator_rtl_match.py
@@ -1,19 +1,10 @@
-import numpy as np
-
 from pyha.common.hwsim import HW
 from pyha.common.sfix import Sfix, right_index, left_index, resize
-from pyha.simulation.simulation_interface import SIM_HW_MODEL, Simulation, SIM_RTL
+from pyha.simulation.simulation_interface import SIM_HW_MODEL, SIM_RTL, assert_sim_match
 
 
-def assert_sim_match(model, types, expected, *x):
-    dut = Simulation(SIM_HW_MODEL, model=model, input_types=types)
-    hw_y = dut.main(*x)
-
-    assert np.allclose(expected, hw_y)
-
-    dut = Simulation(SIM_RTL, model=model, input_types=types)
-    rtl_y = dut.main(*x)
-    assert np.allclose(rtl_y, hw_y)
+def assert_match(model, types, expected, *x):
+    assert_sim_match(model, types, expected, *x, simulations=[SIM_HW_MODEL, SIM_RTL])
 
 
 def test_shift_right():
@@ -24,8 +15,8 @@ def test_shift_right():
 
     x = [2.5, 3.0, -0.54, -2.123]
 
-    assert_sim_match(t0(), [Sfix(left=4, right=-18), int], [1.25, 1.5, -0.27000046, -1.06150055], x, [1] * len(x))
-    assert_sim_match(t0(), [Sfix(left=4, right=-18), int], [0.625, 0.75, -0.13500023, -0.53075027], x, [2] * len(x))
+    assert_match(t0(), [Sfix(left=4, right=-18), int], [1.25, 1.5, -0.27000046, -1.06150055], x, [1] * len(x))
+    assert_match(t0(), [Sfix(left=4, right=-18), int], [0.625, 0.75, -0.13500023, -0.53075027], x, [2] * len(x))
 
 
 def test_right_index():
@@ -34,8 +25,8 @@ def test_right_index():
             ret = right_index(x)
             return ret
 
-    assert_sim_match(t1(), [Sfix(left=4, right=-18)], [-18, -18], [0., 0.])
-    assert_sim_match(t1(), [Sfix(left=4, right=-6)], [-6, -6], [0., 0.])
+    assert_match(t1(), [Sfix(left=4, right=-18)], [-18, -18], [0., 0.])
+    assert_match(t1(), [Sfix(left=4, right=-6)], [-6, -6], [0., 0.])
 
 
 def test_left_index():
@@ -44,8 +35,8 @@ def test_left_index():
             ret = left_index(x)
             return ret
 
-    assert_sim_match(t2(), [Sfix(left=4, right=-18)], [4, 4], [0., 0.])
-    assert_sim_match(t2(), [Sfix(left=0, right=-18)], [0, 0], [0., 0.])
+    assert_match(t2(), [Sfix(left=4, right=-18)], [4, 4], [0., 0.])
+    assert_match(t2(), [Sfix(left=0, right=-18)], [0, 0], [0., 0.])
 
 
 def test_sfix_add():
@@ -57,7 +48,7 @@ def test_sfix_add():
     x = [0.5, -1.2, 1.9, 0.0052]
     x1 = [1.2, -0.2, -2.3, 0.000123]
     expect = [1.7, -1.4, -0.4, 0.0053215]
-    assert_sim_match(t3(), [Sfix(left=4, right=-18)] * 2, expect, x, x1)
+    assert_match(t3(), [Sfix(left=4, right=-18)] * 2, expect, x, x1)
 
 
 def test_sfix_sub():
@@ -69,7 +60,7 @@ def test_sfix_sub():
     x = [0.5, -1.2, 1.9, 0.0052]
     x1 = [1.2, -0.2, -2.3, 0.000123]
     expect = [-0.70000076, -1., 4.20000076, 0.00507736]
-    assert_sim_match(t4(), [Sfix(left=4, right=-18)] * 2, expect, x, x1)
+    assert_match(t4(), [Sfix(left=4, right=-18)] * 2, expect, x, x1)
 
 
 def test_resize_right():
@@ -80,7 +71,7 @@ def test_resize_right():
 
     x = [1.352, 0.5991, -1.123]
     expect = [1.375, 0.625, -1.125]
-    assert_sim_match(t5(), [Sfix(left=2, right=-18)], expect, x)
+    assert_match(t5(), [Sfix(left=2, right=-18)], expect, x)
 
 
 def test_resize_left():
@@ -91,7 +82,7 @@ def test_resize_left():
 
     x = [1.352, 3.0, -1.9]
     expect = [1.0, 1.0, -1.0]
-    assert_sim_match(t6(), [Sfix(left=2, right=-18)], expect, x)
+    assert_match(t6(), [Sfix(left=2, right=-18)], expect, x)
 
 
 def test_array_indexing():
@@ -104,7 +95,7 @@ def test_array_indexing():
 
     x = [0, 1, 2, 3]
     expect = [0.1, 0.2, 0.3, 0.4]
-    assert_sim_match(t7(), [int], expect, x)
+    assert_match(t7(), [int], expect, x)
 
     # test indexing by -1
     class t8(HW):
@@ -116,4 +107,4 @@ def test_array_indexing():
 
     x = [0, 1]
     expect = [0.4, 0.4]
-    assert_sim_match(t8(), [int], expect, x)
+    assert_match(t8(), [int], expect, x)
