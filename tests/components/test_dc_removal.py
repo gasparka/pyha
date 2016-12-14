388cb825d4beacf27b0018bf1998ac972143c258
petspats <gkarm@live.com>
2016-12-14 17:41:35 +0200

    comm

--- tests/components/test_dc_removal.py
+++ tests/components/test_dc_removal.py
@@ -10,5 +10,6 @@ def test_basic():
     expected = [1.75, 1.5, 1.25, 1., 0.75, 0.5, 0.25, 0., -3.5,
               -3., -2.5, -2., -1.5, -1., -0.5, 0.]
     dut = DCRemoval(8)
+
     assert_sim_match(dut, [Sfix(left=2, right=-18)], expected, x,
                      simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL])
