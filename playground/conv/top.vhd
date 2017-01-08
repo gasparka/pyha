62a8571273b9f8c239a7db56122fd99a20aa6ba6
petspats <gkarm@live.com>
2017-01-08 22:15:43 +0200

    fix build

--- playground/conv/top.vhd
+++ playground/conv/top.vhd
@@ -13,37 +13,34 @@ entity  top is
         clk, rst_n: in std_logic;
 
         -- inputs
-        in0: in std_logic_vector(35 downto 0);
-        in1: in std_logic_vector(35 downto 0);
+        in0: in std_logic_vector(19 downto 0);
 
         -- outputs
-        out0: out std_logic_vector(35 downto 0)
+        out0: out std_logic_vector(19 downto 0)
     );
 end entity;
 
 architecture arch of top is
 begin
     process(clk, rst_n)
-        variable self: ComplexMultiply_1.register_t;
+        variable self: DCRemoval_0.register_t;
         -- input variables
-        variable var_in0: complex_sfix0_17;
-        variable var_in1: complex_sfix0_17;
+        variable var_in0: sfixed(1 downto -18);
 
         --output variables
-        variable var_out0: complex_sfix0_17;
+        variable var_out0: sfixed(1 downto -18);
     begin
     if (not rst_n) then
-        ComplexMultiply_1.reset(self);
+        DCRemoval_0.reset(self);
     elsif rising_edge(clk) then
         --convert slv to normal types
-        var_in0 := (real=>to_sfixed(in0(35 downto 18), 0, -17), imag=>to_sfixed(in0(17 downto 0), 0, -17));
-        var_in1 := (real=>to_sfixed(in1(35 downto 18), 0, -17), imag=>to_sfixed(in1(17 downto 0), 0, -17));
+        var_in0 := to_sfixed(in0, 1, -18);
 
         --call the main entry
-        ComplexMultiply_1.main(self, var_in0, var_in1, ret_0=>var_out0);
+        DCRemoval_0.main(self, var_in0, ret_0=>var_out0);
 
         --convert normal types to slv
-        out0 <= to_slv(var_out0.real) & to_slv(var_out0.imag);
+        out0 <= to_slv(var_out0);
       end if;
 
     end process;
