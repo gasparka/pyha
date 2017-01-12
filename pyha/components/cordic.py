9299ed8adb5d101226cf8cfe69e3a960716d852c
petspats <gkarm@live.com>
2017-01-12 21:23:33 +0200

    cordic core passes synth

--- pyha/components/cordic.py
+++ pyha/components/cordic.py
@@ -1,34 +1,47 @@
 import numpy as np
 
 from pyha.common.hwsim import HW
-from pyha.common.sfix import resize, ComplexSfix, Sfix, fixed_truncate
+from pyha.common.sfix import resize, Sfix
 
 
-# class CordicAtom(HW):
-#     def __init__(self):
-#         self.x = Sfix(left=2, right=-17)
-#         self.y = Sfix(left=2, right=-17)
-#         self.phase = Sfix(left=2, right=-17)
-#
-#     def main(self, i, x, y, phase, phase_lut_val):
-#         direction = y < 0
-#         if direction:
-#             self.next.x = resize(x - (y >> i), size_res=x)
-#             self.next.y = resize(y + (x >> i), size_res=y)
-#             self.next.phase = resize(phase - phase_lut_val, size_res=phase)
-#         else:
-#             self.next.x = resize(x + (y >> i), size_res=x)
-#             self.next.y = resize(y - (x >> i), size_res=y)
-#             self.next.phase = resize(phase + phase_lut_val, size_res=phase)
-#
-#         return self.x, self.y, self.phase
+class CordicAtom(HW):
+    def __init__(self):
+        self.x = Sfix()
+        self.y = Sfix()
+        self.phase = Sfix()
+
+    def main(self, i, x, y, phase, phase_adj):
+        direction = y < 0
+        if direction:
+            self.next.x = resize(x - (y >> i), size_res=x)
+            self.next.y = resize(y + (x >> i), size_res=y)
+            self.next.phase = resize(phase - phase_adj, size_res=phase)
+        else:
+            self.next.x = resize(x + (y >> i), size_res=x)
+            self.next.y = resize(y - (x >> i), size_res=y)
+            self.next.phase = resize(phase + phase_adj, size_res=phase)
+
+        return self.x, self.y, self.phase
+
+    def get_delay(self):
+        return 1
+
+    def model_main(self, ai, ax, ay, aphase, aphase_adj):
+        res = []
+        for i, x, y, phase, phase_adj in zip(ai, ax, ay, aphase, aphase_adj):
+            sign = 1 if y < 0 else -1
+            x, y, phase = x - sign * (y * (2 ** -i)), y + sign * (x * (2 ** -i)), phase - sign * phase_adj
+            res.append([x, y, phase])
+        return res
 
 
 class CordicCore(HW):
     def __init__(self, iterations):
         self.iterations = iterations
-        self.phase_lut = [np.arctan(2 ** -i) for i in range(iterations)]
-        self.phase_lut_fix = [Sfix(x, 0, -iterations) for x in self.phase_lut]
+
+        self.iterations = iterations + 1
+        self.phase_lut = [np.arctan(2 ** -i) for i in range(self.iterations)]
+        self.phase_lut_fix = [Sfix(x, 0, -17) for x in self.phase_lut]
 
         # pipeline registers
         self.x = [Sfix(left=2, right=-17)] * self.iterations
@@ -61,7 +74,9 @@ class CordicCore(HW):
     #     return resize(xn, size_res=x), resize(yn, size_res=y), resize(pn, size_res=p)
 
     def main(self, c, phase):
-        self.next.x[0], self.next.y[0], self.next.phase[0] = c.real, c.imag, phase
+        self.next.x[0] = c.real
+        self.next.y[0] = c.imag
+        self.next.phase[0] = phase
 
         # for i in range(len(self.phase_lut_fix) - 1):
         #     self.next.x[i + 1], self.next.y[i + 1], self.next.phase[i + 1] = \
--- tests/components/test_cordic.py
+++ tests/components/test_cordic.py
@@ -1,11 +1,34 @@
-from pyha.common.sfix import ComplexSfix, Sfix
-from pyha.components.cordic import CordicCore
-from pyha.simulation.simulation_interface import assert_sim_match, SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE
 import numpy as np
+
+from pyha.common.sfix import ComplexSfix, Sfix
+from pyha.components.cordic import CordicCore, CordicAtom
+from pyha.simulation.simulation_interface import assert_sim_match, SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE
+
+
+def test_atom():
+    phase_lut = [np.arctan(2 ** -i) for i in range(32)]
+    i = [0, 1, 2, 3, 4, 5]
+    x = [0.5, 0.2, 0.1, 0.99]
+    y = [1., 0.87, 0.0, 0.56]
+    phase = [0] * len(i)
+    phase_adj = [phase_lut[x] for x in i]
+
+    inputs = [i, x, y, phase, phase_adj]
+    expect = [[1.5, 0.635, 0.1, 1.06],
+              [0.5, 0.77, -0.025, 0.43625],
+              [0.785398, 0.463648, 0.244979, 0.124355]]
+
+    dut = CordicAtom()
+    assert_sim_match(dut, [int] + [Sfix(left=2, right=-17)] * 4, expect, *inputs,
+                     simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE],
+                     rtol=1e-4
+                     # dir_path='/home/gaspar/git/pyha/playground/conv'
+
+                     )
 
 
 def test_core_vectoring():
-    inputs = [0.5 + 0.1j, 1+0j, 0+1j, 0.234+0.9j]
+    inputs = [0.5 + 0.1j, 1 + 0j, 0 + 1j, 0.234 + 0.9j]
     phase = [0.0] * len(inputs)
 
     ang = np.angle(inputs)
@@ -16,6 +39,7 @@ def test_core_vectoring():
     assert_sim_match(dut, [ComplexSfix(left=2, right=-17), Sfix(left=2, right=-17)],
                      expect, inputs, phase,
                      rtol=1e-5,
-                     atol=1e-4, #zeroes make trouble
-                     simulations=[SIM_MODEL, SIM_HW_MODEL]
+                     atol=1e-4,  # zeroes make trouble
+                     simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE],
+                     dir_path='/home/gaspar/git/pyha/playground/conv'
                      )
