2b1375fe4ad3919c2d59098e39f01fc587cad7ca
petspats <gkarm@live.com>
2017-01-19 20:53:04 +0200

    cleanup

--- tests/components/test_cordic.py
+++ tests/components/test_cordic.py
@@ -8,66 +8,6 @@ from pyha.simulation.simulation_interface import assert_sim_match, SIM_MODEL, SI
     assert_hwmodel_rtl_match, assert_model_hwmodel_match
 
 
-# TODO: model not tied to hw_model
-# def test_atom():
-#     phase_lut = [np.arctan(2 ** -i) for i in range(32)]
-#     i = [0, 1, 2, 3, 4, 5]
-#     x = [0.5, 0.2, 0.1, 0.99]
-#     y = [1., 0.87, 0.0, 0.56]
-#     phase = [0] * len(i)
-#     phase_adj = [phase_lut[x] for x in i]
-#
-#     inputs = [i, x, y, phase, phase_adj]
-#     expect = [[1.5, 0.635, 0.1, 1.06],
-#               [0.5, 0.77, -0.025, 0.43625],
-#               [0.785398, 0.463648, 0.244979, 0.124355]]
-#
-#     dut = CordicAtom()
-#     assert_sim_match(dut, [int] + [Sfix(left=2, right=-17)] * 4, expect, *inputs,
-#                      simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE],
-#                      rtol=1e-4,
-#                      atol=1e-5,
-#                      dir_path='/home/gaspar/git/pyha/playground/conv'
-#
-#                      )
-#
-#
-# def test_core_alt_vectoring():
-#     inputs = [0.5 + 0.1j, 1 + 0j, 0 + 1j, 0.234 + 0.9j]
-#     phase = [0.0] * len(inputs)
-#
-#     ang = np.angle(inputs)
-#     abs = np.abs(inputs)
-#     expect = [abs * 1.646760, [0.0] * len(inputs), ang]
-#     dut = CordicCoreAlt(iterations=18)
-#
-#     assert_sim_match(dut, [ComplexSfix(left=2, right=-17), Sfix(left=2, right=-17)],
-#                      expect, inputs, phase,
-#                      rtol=1e-4,
-#                      atol=1e-4,  # zeroes make trouble
-#                      simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE],
-#                      dir_path='/home/gaspar/git/pyha/playground/conv'
-#                      )
-#
-#
-# def test_core_vectoring():
-#     inputs = [0.5 + 0.1j, 1 + 0j, 0 + 1j, 0.234 + 0.9j, +0.9 - 0.9j]
-#     phase = [0.0] * len(inputs)
-#
-#     ang = np.angle(inputs)
-#     abs = np.abs(inputs)
-#     expect = [abs * 1.646760, [0.0] * len(inputs), ang]
-#     dut = CordicCore(iterations=17)
-#
-#     assert_sim_match(dut, [ComplexSfix(left=0, right=-17), Sfix(left=2, right=-17)],
-#                      expect, inputs, phase,
-#                      rtol=1e-5,
-#                      atol=1e-4,  # zeroes make trouble
-#                      simulations=[SIM_MODEL, SIM_HW_MODEL, SIM_RTL, SIM_GATE],
-#                      dir_path='/home/gaspar/git/pyha/playground/conv'
-#                      )
-
-
 def test_polar_quadrant_i():
     inputs = [0.234 + 0.92j]
     expect = [np.abs(inputs), np.angle(inputs) / np.pi]
