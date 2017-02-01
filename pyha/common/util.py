c7291eba6fd4ee665ae73c4aa70d8f39dff3c787
petspats <gkarm@live.com>
2017-02-01 19:26:22 +0200

    add test

--- pyha/common/util.py
+++ pyha/common/util.py
@@ -102,3 +102,4 @@ def test_bools_to_hex():
     assert bools_to_hex([True, True, True, True]) == '0xf'
     assert bools_to_hex([True, True]) == '0x3'
     assert bools_to_hex([False, True, True, True, True, False, True, True]) == '0x7b'
+    assert bools_to_hex([0, 1, 1, 1, 1, 0, 1, 1]) == '0x7b'
