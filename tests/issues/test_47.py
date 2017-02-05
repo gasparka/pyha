34d08cc20d287fdf939dba7922bb093996ea25a6
petspats <gkarm@live.com>
2017-02-05 19:50:53 +0200

    test full

--- tests/issues/test_47.py
+++ tests/issues/test_47.py
@@ -54,51 +54,55 @@ class TestBasic:
         dm = self.conversion.get_headers()
         assert expect == dm
 
-        # def test_full(self):
-        #     expect = textwrap.dedent("""\
-        #        package B0_0 is
-        #
-        #
-        #
-        #            type register_t is record
-        #                much_dummy_very_wow: integer;
-        #            end record;
-        #            type self_t is record
-        #                much_dummy_very_wow: integer;
-        #                \\next\\: register_t;
-        #            end record;
-        #
-        #            procedure reset(self_reg: inout register_t);
-        #            procedure main(self_reg:inout register_t; new_value: sfixed(0 downto -27); ret_0:out sfixed(0 downto -27));
-        #            procedure get_delay(self:inout self_t; ret_0:out integer);
-        #        end package;
-        #
-        #        package body B0_0 is
-        #            procedure reset(self_reg: inout register_t) is
-        #            begin
-        #                self_reg.a := Sfix(0.0, 0, -27);
-        #            end procedure;
-        #
-        #            procedure make_self(self_reg: register_t; self: out self_t) is
-        #            begin
-        #                self.a := self_reg.a;
-        #                self.\\next\\ := self_reg;
-        #            end procedure;
-        #
-        #            procedure main(self_reg:inout register_t; new_value: sfixed(0 downto -27); ret_0:out sfixed(0 downto -27)) is
-        #                variable self: self_t;
-        #            begin
-        #                make_self(self_reg, self);
-        #                self.\\next\\.a := new_value;
-        #                ret_0 := self.a;
-        #                self_reg := self.\\next\\;
-        #            end procedure;
-        #
-        #            procedure get_delay(self:inout self_t; ret_0:out integer) is
-        #            begin
-        #                ret_0 := 1;
-        #            end procedure;
-        #            end package body;""")
-        #
-        #     conv = str(self.conversion)
-        #     assert expect == conv[conv.index('package'):]
+    def test_full(self):
+        expect = textwrap.dedent("""\
+            package B0_0 is
+
+
+
+                type register_t is record
+                    much_dummy_very_wow: integer;
+                end record;
+                type self_t is record
+                    much_dummy_very_wow: integer;
+                    \\next\\: register_t;
+                end record;
+
+                procedure reset(self_reg: inout register_t);
+                procedure main(self_reg:inout register_t; a: boolean; ret_0:out integer);
+                procedure main_user(self:inout self_t; a: boolean; ret_0:out integer);
+            end package;
+
+            package body B0_0 is
+                procedure reset(self_reg: inout register_t) is
+                begin
+                    self_reg.much_dummy_very_wow := 0;
+                end procedure;
+
+                procedure make_self(self_reg: register_t; self: out self_t) is
+                begin
+                    self.much_dummy_very_wow := self_reg.much_dummy_very_wow;
+                    self.\\next\\ := self_reg;
+                end procedure;
+
+                procedure main(self_reg:inout register_t; a: boolean; ret_0:out integer) is
+                    variable self: self_t;
+                begin
+                    make_self(self_reg, self);
+                    main_user(self, a, ret_0);
+                    self_reg := self.\\next\\;
+                end procedure;
+
+                procedure main_user(self:inout self_t; a: boolean; ret_0:out integer) is
+                begin
+                    if a then
+                        ret_0 := 1;
+                        return;
+                    end if;
+                    ret_0 := 2;
+                    return;
+                end procedure;
+            end package body;""")
+
+        conv = str(self.conversion)
+        assert expect == conv[conv.index('package'):]
