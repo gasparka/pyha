ef18328a1c05ac2f0791436d484930746f65b49a
petspats <gkarm@live.com>
2017-03-07 18:28:50 +0200

    list resize tests

--- tests/test_autoresize.py
+++ tests/test_autoresize.py
@@ -101,3 +101,48 @@ class TestSfixList:
         assert dut.next.a[1].left == 0
         assert dut.next.a[1].right == -4
         assert dut.next.a[1].val == 0.125
+
+    def test_round(self):
+        x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        expected = [
+            [0.0, 0.125, 0.1875, 0.3125, 0.375, 0.5, 0.625, 0.6875, 0.8125, 0.875],
+            [0.0, 0.125, 0.1875, 0.3125, 0.375, 0.5, 0.625, 0.6875, 0.8125, 0.875]
+            ]
+
+        dut = self.A(fixed_saturate, fixed_round)
+        assert_sim_match(dut, [Sfix(left=0, right=-17)], expected, x,
+                         simulations=[SIM_HW_MODEL])
+
+    def test_truncate(self):
+        x = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+        expected = [
+            [0.0, 0.0625, 0.1875, 0.25, 0.375, 0.5, 0.5625, 0.6875, 0.75, 0.875],
+            [0.0, 0.0625, 0.1875, 0.25, 0.375, 0.5, 0.5625, 0.6875, 0.75, 0.875]
+            ]
+
+        dut = self.A(fixed_saturate, fixed_truncate)
+        assert_sim_match(dut, [Sfix(left=0, right=-17)], expected, x,
+                         simulations=[SIM_HW_MODEL])
+
+    def test_saturation(self):
+        x = [0.9, 1.0, 1.5, 2.0]
+        expected = [
+            [0.875, 0.9375, 0.9375, 0.9375],
+            [0.875, 0.9375, 0.9375, 0.9375]
+            ]
+
+        dut = self.A(fixed_saturate, fixed_truncate)
+        assert_sim_match(dut, [Sfix(left=2, right=-17)], expected, x,
+                         simulations=[SIM_HW_MODEL])
+
+    def test_wrap(self):
+        x = [0.9, 1.0, 1.5, 2.0]
+        expected = [
+            [0.875, -1, -0.5, 0],
+            [0.875, -1, -0.5, 0]
+            ]
+
+        dut = self.A(fixed_wrap, fixed_truncate)
+        assert_sim_match(dut, [Sfix(left=2, right=-17)], expected, x,
+                         simulations=[SIM_HW_MODEL])
