63b415b5e63d9ef5e77af9f2a31ac7622a7f9078
petspats <gkarm@live.com>
2017-05-11 12:26:06 +0300

    commend out print statements

--- pyha/simulation/cocotb_simulation_top.py
+++ pyha/simulation/cocotb_simulation_top.py
@@ -6,6 +6,7 @@ from cocotb.clock import Clock
 from cocotb.result import ReturnValue
 from cocotb.triggers import RisingEdge, Timer, FallingEdge
 
+debug = False
 
 @cocotb.coroutine
 def reset(dut, duration=10000):
@@ -24,22 +25,19 @@ def run_dut(dut, in_data, out_count):
     cocotb.fork(Clock(dut.clk, 5000).start())
     yield reset(dut)
 
-    # FIXME: test input and output names and report error
     ret = []
-    print('Input data: {}'.format(in_data))
+    # print('Input data: {}'.format(in_data))
     for x in in_data:
 
 
 
         # put input
-        print('Processing slice: {}'.format(x))
+        # print('Processing slice: {}'.format(x))
         for i, xi in enumerate(x):
-            print('Set {} to {}'.format('in' + str(i), xi))
+            # print('Set {} to {}'.format('in' + str(i), xi))
             setattr(dut, 'in' + str(i), int(xi.astype(int)))
 
-        # NOTICE: need to have both yields to match simulation.
         yield RisingEdge(dut.clk)
-        # yield FallingEdge(dut.clk)
 
         # collect output
         tmp = []
@@ -47,11 +45,11 @@ def run_dut(dut, in_data, out_count):
             var = 'out' + str(i)
             # val = getattr(dut, var).value.signed_integer
             val = str(getattr(dut, var).value)
-            print(val)
+            # print(val)
             tmp.append(val)
         ret.append(tmp)
 
-    print('Finish, ret: {}'.format(ret))
+    # print('Finish, ret: {}'.format(ret))
     raise ReturnValue(ret)
 
 
@@ -59,12 +57,8 @@ def run_dut(dut, in_data, out_count):
 def test_main(dut):
     import os
     in_data = np.load(os.getcwd() + '/../input.npy')
-    # in_data = np.transpose(in_data)
-    print(in_data)
 
     output_vars = int(os.environ['OUTPUT_VARIABLES'])
     hdl_out = yield run_dut(dut, in_data, output_vars)
-    # hdl_out = np.transpose(hdl_out)
-    # print(hdl_out)
 
     np.save(os.getcwd() + '/../output.npy', hdl_out)
