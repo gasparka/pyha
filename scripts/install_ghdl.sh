6a33ddcd77ddb97a5d7fd2f345a4861f85e287f7
gaspar <gkarm@live.com>
2017-11-02 20:46:10 +0200

    move to latest GHDL

--- scripts/install_ghdl.sh
+++ scripts/install_ghdl.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -vx
 
-# install gnat runtime, adding repo is needed for Ubuntu < 14
-echo deb http://cz.archive.ubuntu.com/ubuntu precise main universe precise main universe > /etc/apt/sources.list.d/pyha_tmp.list
+# install gnat runtime, adding repo is needed if not ran on  Ubuntu 16 (Xenial)
+echo deb http://cz.archive.ubuntu.com/ubuntu xenial main universe > /etc/apt/sources.list.d/pyha_tmp.list
 apt-get update
-apt-get install -y gnat-4.6-base libgnat-4.6
+apt-get install -y libgnat-4.9
 rm -f /etc/apt/sources.list.d/pyha_tmp.list
 
 # download ghdl
-wget https://github.com/tgingold/ghdl/releases/download/2016-09-14/ghdl-0.34dev-mcode-2016-09-14.tgz -O /tmp/ghdl.tar.gz
+wget https://github.com/tgingold/ghdl/releases/download/v0.34/ghdl-v0.34-mcode-ubuntu.tgz -O /tmp/ghdl.tar.gz
 mkdir ghdl
 tar -C ghdl -xvf /tmp/ghdl.tar.gz
 
-#export PATH=$PATH:$PWD/ghdl/bin/
-#echo 'export PATH=$PATH:$PWD/ghdl/bin/' >> ~/.bashrc
-#source $HOME/.bashrc
+echo 'export PATH=$PATH:$PWD/ghdl/bin/' >> ~/.bashrc
+
