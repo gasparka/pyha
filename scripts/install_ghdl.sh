da92d96a0b4978ef9863b89e013ac9556e015188
gaspar <gkarm@live.com>
2017-12-11 19:15:25 +0200

    install_ghdl: needed more sudos

--- scripts/install_ghdl.sh
+++ scripts/install_ghdl.sh
@@ -2,10 +2,13 @@
 set -vx
 
 # install gnat runtime, adding repo is needed if not ran on  Ubuntu 16 (Xenial)
-echo deb http://cz.archive.ubuntu.com/ubuntu xenial main universe > /etc/apt/sources.list.d/pyha_tmp.list
+sudo sh -c 'echo deb http://cz.archive.ubuntu.com/ubuntu xenial main universe > /etc/apt/sources.list.d/pyha_tmp.list'
 sudo apt-get update
 sudo apt-get install -y libgnat-4.9
-rm -f /etc/apt/sources.list.d/pyha_tmp.list
+
+# get rid of the xenial repo
+sudo rm -f /etc/apt/sources.list.d/pyha_tmp.list
+sudo apt-get update
 
 # download ghdl
 wget https://github.com/tgingold/ghdl/releases/download/v0.34/ghdl-v0.34-mcode-ubuntu.tgz -O /tmp/ghdl.tar.gz
