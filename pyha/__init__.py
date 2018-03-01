aec98d0d3bf0907400ccf9471476a412306658e0
gaspar <gkarm@live.com>
2018-03-01 13:08:24 +0200

    init: import redbaron with 'ipython_behaviour = False', otherwise running from ipython wont work!

--- pyha/__init__.py
+++ pyha/__init__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import redbaron
+
 from .common.complex import Complex
 from .common.core import Hardware
 from .common.fixed_point import Sfix, scalb, resize, left_index, right_index
 from .simulation.simulation_interface import simulate, assert_equals, sims_close, hardware_sims_equal
+
+redbaron.ipython_behavior = False
 
 __author__ = """"""
 __email__ = ''
