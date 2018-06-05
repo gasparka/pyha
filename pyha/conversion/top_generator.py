cc7b9918db38c5eafbe21c1fc79455804df16dad
Gaspar Karm <gkarm@live.com>
2018-06-05 14:08:28 +0300

    fix build: dont import mpmath globally, not in requirements!

--- pyha/conversion/top_generator.py
+++ pyha/conversion/top_generator.py
@@ -5,7 +5,6 @@ from pyha.common.util import tabber, formatter, is_constant, const_filter
 from pyha.conversion.python_types_vhdl import VHDLModule, init_vhdl_type, VHDLList, escape_reserved_vhdl
 from pyha.conversion.redbaron_mods import file_header
 
-from mpmath import mpf
 class NotTrainedError(Exception):
     pass
 
