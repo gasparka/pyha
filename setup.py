a5e4f85b247e01450ad19118fd97db0f8a45c27b
gaspar <gkarm@live.com>
2018-08-04 14:43:04 +0300

    setup: fix pip regression

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@
 import os
 import sys
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup, find_packages
 
 if sys.version_info < (3, 6):
