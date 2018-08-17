f36ec29b91c49ab988f664738185a4a61f00bb93
Gaspar Karm <gkarm@live.com>
2018-08-17 14:24:36 +0300

    refactor away util file

--- pyha/__init__.py
+++ pyha/__init__.py
@@ -6,7 +6,7 @@ import redbaron
 from .common.complex import Complex
 from .common.core import Hardware
 from .common.fixed_point import Sfix, scalb, resize, left_index, right_index
-from .simulation.simulation_interface import simulate, assert_equals, sims_close, hardware_sims_equal
+from .simulation.simulation_interface import simulate, assert_equals, sims_close, hardware_sims_equal, get_resource_usage
 from .conversion.conversion import convertToVHDL
 
 redbaron.ipython_behavior = False
