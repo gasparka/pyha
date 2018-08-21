64cc0fa2916671aeef0a18690d5eff826bee4442
Gaspar Karm <gkarm@live.com>
2018-08-21 12:27:24 +0300

    modernize moving average

--- pyha/cores/__init__.py
+++ pyha/cores/__init__.py
@@ -1,3 +1,4 @@
 from .cordic.core import Cordic, CordicMode
 from .cordic.to_polar.to_polar import ToPolar, Angle, Abs
 from .cordic.nco.nco import NCO
+from .filter import FIR, MovingAverage, DCRemoval
--- pyha/cores/filter/moving_average/moving_average.py
+++ pyha/cores/filter/moving_average/moving_average.py
@@ -1,34 +1,26 @@
 from scipy import signal
-
 import pytest
 from pyha import Hardware, Sfix, simulate, sims_close, Complex, resize, scalb
-from pyha.common.util import is_power2
 import numpy as np
 
 
 class MovingAverage(Hardware):
     """
-    :param window_len: Size of the moving average window, must be power of 2
+    :param window_len: Size of the moving average window, must be power of 2 and >= 2
     :param dtype: internal storage type, Sfix/Complex
     """
 
     def __init__(self, window_len, dtype=Sfix):
-        if window_len < 2:
-            raise AttributeError('Window length must be >= 2')
-
-        if not is_power2(window_len):
-            raise AttributeError('Window length must be power of 2')
-
         self.WINDOW_LEN = window_len
-        self.WINDOW_POW = int(np.log2(window_len))
+        self.BIT_GROWTH = int(np.log2(window_len))
         self.DELAY = 1
 
         self.mem = [dtype()] * self.WINDOW_LEN
-        self.sum = dtype(0.0, 0, -17 - self.WINDOW_POW)
+        self.sum = dtype(0.0, 0, -17 - self.BIT_GROWTH)
 
     def main(self, new_sample):
         # add new element to shift register
-        scaled = scalb(new_sample, -self.WINDOW_POW)
+        scaled = scalb(new_sample, -self.BIT_GROWTH)
         self.mem = [scaled] + self.mem[:-1]
 
         self.sum = self.sum + scaled - self.mem[-1]
@@ -40,65 +32,19 @@ class MovingAverage(Hardware):
         return signal.lfilter(taps, [1.0], inputs)
 
 
-def test_incorrect_conf():
-    with pytest.raises(AttributeError):
-        mov = MovingAverage(window_len=1)  # too small window
-
-    with pytest.raises(AttributeError):
-        mov = MovingAverage(window_len=3)  # not power of 2
-
-
-def test_window2():
-    dut = MovingAverage(window_len=2)
-    x = [0.0, 0.1, 0.2, 0.3, 0.4]
-    expected = [0.0, 0.05, 0.15, 0.25, 0.35]
-    sim_out = simulate(dut, x)
-    assert sims_close(sim_out, expected)
-
-
-def test_window4():
-    dut = MovingAverage(window_len=4)
-    x = [-0.2, 0.05, 1.0, -0.9571, 0.0987]
-    expected = [-0.05, -0.0375, 0.2125, -0.026775, 0.0479]
-    sim_out = simulate(dut, x)
-    assert sims_close(sim_out, expected)
-
-
-def test_max():
-    dut = MovingAverage(window_len=4)
-    x = [1., 1., 1., 1., 1., 1.]
-    expected = [0.25, 0.5, 0.75, 1., 1., 1.]
-    sim_out = simulate(dut, x)
-    assert sims_close(sim_out, expected)
-
-
-def test_min():
-    dut = MovingAverage(window_len=8)
-    x = [-1., -1., -1., -1., -1., -1., -1., -1., -1.]
-    expected = [-0.125, -0.25, -0.375, -0.5, -0.625, -0.75, -0.875, -1., -1.]
-    sim_out = simulate(dut, x)
-    assert sims_close(sim_out, expected)
-
-
-def test_noisy_signal():
+@pytest.mark.parametrize("window_len", [2, 4, 8, 16, 32])
+@pytest.mark.parametrize("input_power", [0.25, 0.001, 0.0001])
+@pytest.mark.parametrize("dtype", [Sfix, Complex])
+def test_all(window_len, input_power, dtype):
     np.random.seed(0)
-    dut = MovingAverage(window_len=8)
-    x = np.linspace(0, 2 * 2 * np.pi, 512)
-    y = 0.7 * np.sin(x)
-    noise = 0.1 * np.random.normal(size=512)
-    y += noise
+    dut = MovingAverage(window_len=window_len, dtype=dtype)
+    N = 128
+    if dtype == Complex:
+        input_signal = (np.random.normal(size=N) + np.random.normal(size=N) * 1j)
+    else:
+        input_signal = np.random.normal(size=N)
 
-    sim_out = simulate(dut, y)
-    assert sims_close(sim_out, atol=1e-4)
+    input_signal *= input_power
 
-
-def test_complex():
-    np.random.seed(0)
-    dut = MovingAverage(window_len=128, dtype=Complex)
-    N = 2 ** 13
-    x = (np.random.normal(size=N) + np.random.normal(size=N) * 1j) * 0.25
-
-    x_quantized = [Complex(x, 0, -17).val for x in x]
-
-    sim_out = simulate(dut, x_quantized, simulations=['MODEL', 'PYHA'])
-    assert sims_close(sim_out, rtol=1e-4, atol=1e-4)
+    sim_out = simulate(dut, input_signal)
+    assert sims_close(sim_out)
