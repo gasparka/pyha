6e47f9784f85e524d7526cb3f3abdfc040aadb47
Gaspar Karm <gkarm@live.com>
2018-08-22 19:28:37 +0300

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extra_files = package_files('pyha/simulation/sim_include/')
 
 setup(
     name='pyha',
-    version='0.0.9',
+    version='0.0.10',
     description="Pyha",
     long_description=readme,
     author="Gaspar Karm",
