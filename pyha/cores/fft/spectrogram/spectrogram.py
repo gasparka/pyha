averager and dc-removal fixes

pyha/cores/fft/spectrogram/spectrogram.py
@@ -1,7 +1,9 @@
 import logging
+
 import numpy as np
 import pytest
 from scipy.signal import get_window
+
 from pyha import Hardware, simulate, sims_close, Complex
 from pyha.cores import DCRemoval, Packager, Windower, R2SDF, FFTPower, BitreversalFFTshiftAVGPool, \
     DataIndexValidDePackager
@@ -41,6 +43,8 @@ class Spectrogram(Hardware):
 
     def model_main(self, x):
         no_dc = x - np.mean(x)
+        no_dc = x
+        no_dc = self.dc_removal.model_main(x)
         resh = np.reshape(no_dc, (-1, self.FFT_SIZE))
         windowed = resh * get_window(self.WINDOW_TYPE, self.FFT_SIZE)
         transform = np.fft.fft(windowed) / self.FFT_SIZE
@@ -71,7 +75,7 @@ def test_all(fft_size, avg_freq_axis, avg_time_axis, input_power):
 
     orig_inp_quant = np.vectorize(lambda x: complex(Complex(x, 0, -17)))(orig_inp)
     sims = simulate(dut, orig_inp_quant, simulations=['MODEL', 'PYHA'])
-    assert sims_close(sims, rtol=1e-6, atol=1e-6)
+    assert sims_close(sims, rtol=1e-5, atol=1e-5) # without DC removal, 1e-6 passes
 
 
 def test_simple():

pyha/cores/filter/dc_removal/dc_removal.py
@@ -1,39 +1,57 @@
+import numpy as np
 import pytest
 
 from pyha import Hardware, Sfix, simulate, sims_close, Complex
-import numpy as np
-
-from pyha.cores import MovingAverage
+from pyha.common.shift_register import ShiftRegister
+from pyha.cores import MovingAverage, DataIndexValidPackager, DataIndexValidDePackager
 
 
 class DCRemoval(Hardware):
     """
     Filter out DC component, based on: https://www.dsprelated.com/showarticle/58.php
-    Deviation from the article that there is no delay matching, which in my opinion is ~useless as the DC component is more or less stable.
     """
     def __init__(self, window_len, dtype=Sfix):
-        self.mavg = [MovingAverage(window_len, dtype), MovingAverage(window_len, dtype),
-                     MovingAverage(window_len, dtype), MovingAverage(window_len, dtype)]
+        self._pyha_simulation_input_callback = DataIndexValidPackager(
+            dtype=dtype(0.0, 0, -17, overflow_style='saturate'))
+        self._pyha_simulation_output_callback = DataIndexValidDePackager()
 
+        self.WINDOW_LEN = window_len
+        self.averages = [MovingAverage(window_len, dtype), MovingAverage(window_len, dtype)]
+
+        self.DELAY = self.averages[0].DELAY * len(self.averages) + 1
+
+        # input must be delayed by group delay, but we can use the SHR from the first averager to get the majority of the delay.
+        self.input_shr = ShiftRegister([dtype(0.0, 0, -17)] * (self.DELAY - 2))
         self.y = dtype(0, 0, -17)
 
-        self.DELAY = 1
-
     def main(self, x):
+        # delay input
+        first_delay = self.averages[0].shr.peek()
+        self.input_shr.push_next(first_delay)
+
         # run input signal over all the MA's
         var = x
-        for mav in self.mavg:
+        for mav in self.averages:
             var = mav.main(var)
 
         # dc-free signal
-        self.y = x - var
+        out = self.input_shr.peek()
+        if not var.valid:
+            self.y = out
+        else:
+            self.y = out - var.data
         return self.y
 
-    def model_main(self, xl):
-        tmp = xl
-        for mav in self.mavg:
+    def model_main(self, input_list):
+        input_list = np.array(input_list)
+        tmp = input_list
+        for mav in self.averages:
             tmp = mav.model_main(tmp)
-        y = xl - np.array([0, 0, 0, 0] + tmp.tolist()[:-4])
+
+        # delaying the input is important, without this you get 6db ripple...
+        group_delay = int(len(self.averages) * ((self.WINDOW_LEN - 1) / 2))
+        delayed_input = np.hstack([[0] * group_delay, input_list[:-group_delay]])
+        y = delayed_input - tmp
         return y
 
 

pyha/cores/filter/moving_average/moving_average.py
@@ -1,7 +1,10 @@
-from scipy import signal
-import pytest
-from pyha import Hardware, Sfix, simulate, sims_close, Complex, resize, scalb
 import numpy as np
+import pytest
+from scipy import signal
+
+from pyha import Hardware, Sfix, simulate, sims_close, Complex, scalb
+from pyha.common.shift_register import ShiftRegister
+from pyha.cores import DataIndexValid, DataIndexValidPackager, DataIndexValidDePackager
 
 
 class MovingAverage(Hardware):
@@ -11,20 +14,30 @@ class MovingAverage(Hardware):
     """
 
     def __init__(self, window_len, dtype=Sfix):
+        self._pyha_simulation_input_callback = DataIndexValidPackager(
+            dtype=dtype(0.0, 0, -17, overflow_style='saturate'))
+        self._pyha_simulation_output_callback = DataIndexValidDePackager()
         self.WINDOW_LEN = window_len
         self.BIT_GROWTH = int(np.log2(window_len))
-        self.DELAY = 1
+        self.DELAY = 2
 
-        self.mem = [dtype()] * self.WINDOW_LEN
-        self.sum = dtype(0.0, 0, -17 - self.BIT_GROWTH)
+        self.shr = ShiftRegister([dtype()] * self.WINDOW_LEN)
+        self.acc = dtype(0.0, self.BIT_GROWTH, -17)
 
-    def main(self, new_sample):
+        # rounding the output is necessary or there will be negative trend!
+        self.out = DataIndexValid(dtype(0, 0, -17, round_style='round'), valid=False)
+
+    def main(self, inp):
+        if not inp.valid:
+            return DataIndexValid(self.out.data, 0, valid=False)
+
         # add new element to shift register
-        scaled = scalb(new_sample, -self.BIT_GROWTH)
-        self.mem = [scaled] + self.mem[:-1]
+        self.shr.push_next(inp.data)
 
-        self.sum = self.sum + scaled - self.mem[-1]
-        return resize(self.sum, 0, -17)
+        self.acc = self.acc + inp.data - self.shr.peek()
+        self.out.data = scalb(self.acc, -self.BIT_GROWTH)
+        self.out.valid = True
+        return self.out
 
     def model_main(self, inputs):
         # can be expressed as FIR filter: